3c007556900d6852d2d1727251965d94227f3726
sunlei213 <sunleity@163.com>
2017-03-24 17:29:01 +0800

    神经网络

--- python/pylearn.py
+++ python/pylearn.py
@@ -6,7 +6,7 @@ Created on Mon Mar 28 19:01:49 2016
 """
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.tools.customxml import NetworkWriter
+from pybrain.tools.customxml import NetworkWriter,NetworkReader
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.utilities import percentError
 from pybrain.structure.modules import *
@@ -17,7 +17,7 @@ import time
 HISTORY=20
 label1=5
 
-training_set = ("2013-01-01", "2016-07-01")       # 训练集（六年）
+training_set = ("2016-02-01", "2017-03-17")       # 训练集（六年）
 testing_set  = ("2016-01-05", "2016-12-31")       # 测试集（2015上半年数据）
 
 def sigmoid(X,useStatus=True):  
@@ -75,6 +75,9 @@ def save_arguments(net,filename):
     NetworkWriter.writeToFile(net, filename)
     print 'Arguments save to file net.csv'
 
+def load_arguments(filename):
+    return NetworkReader.readFrom(filename)
+
 ### 构造BP训练实例
 def make_trainer(net, ds, learningrate=0.01,momentum = 0.1, verbose = True, weightdecay = 0.1): # 网络, 训练集, 训练参数
     trainer = BackpropTrainer(net, ds, learningrate=learningrate,momentum = momentum, verbose = verbose, weightdecay = weightdecay)
@@ -93,12 +96,14 @@ def start_testing(net, dataset):
 sl=ts.get_zz500s()
 sl=list(sl['code'])
 universe=sl
+
 #universe=['600030']
 ds=random_data(make_data(training_set))
 training_dataset,testing_dataset = ds.splitWithProportion(0.9)
 #training_dataset = ds
 print(len(ds))
-fnn = buildNetwork(HISTORY, 200, 100,50, 2, bias = True,recurrent=True, hiddenclass=TanhLayer,outclass=SoftmaxLayer )
+fnn = load_arguments('500.xml')
+#fnn = buildNetwork(HISTORY, 200, 100,50, 2, bias = True,recurrent=True, hiddenclass=TanhLayer,outclass=SoftmaxLayer )
 #testing_dataset  = random_data(make_data(testing_set))
 trainer = make_trainer(fnn, training_dataset,0.005,weightdecay = 0.00001)
 s_time=time.time()
@@ -112,17 +117,21 @@ for i in range(100):
 run_time=time.time()-s_time
 print run_time
 
-save_arguments(fnn,'500.xml')
+save_arguments(fnn,'500_1.xml')
 s1=start_testing(fnn, testing_dataset )
-t=f=0
-for i in range(min(len(s1),50)):
-    print s1[i],testing_dataset['target'][i]
-    if s1[i][0]>s1[i][1]:
-        if testing_dataset['target'][i][0]==1:
-            t +=1
-        else:
-            f +=1
-print float(t)/float(t+f)
+t=[0]*10
+f=[0]*10
+for i in range(len(s1)):
+#    print s1[i],testing_dataset['target'][i]
+    x = s1[i][0]-s1[i][1]
+    for l in range(10):
+        if x>float(0.1*l):
+            if testing_dataset['target'][i][0]==1:
+                t[l] +=1
+            else:
+                f[l] +=1
+for l in range(10):
+    print '>0.{0}:命中率：{1:.2f}% ,命中数：{2} ,总数：{3} '.format(l,float(t[l])*100/float(t[l]+f[l]),t[l],t[l]+f[l])
 
 tstresult = percentError( s1, testing_dataset['target'] )
 print tstresult
